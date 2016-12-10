02a83195a8c5309ed8ad71616ab6040fb57ad282
arpanpathak <arpanpathak1996@gmail.com>
2016-12-10 17:22:19 +0530

    Access to submission folder is disabled

--- submissions/null/MyClass.java
+++
@@ -1,8 +0,0 @@
-public class MyClass
-{
-	public static void main(String[] args)
-	{
-		for(int i=0;i<100;i++)
-		System.out.println("hi");
-	}
-}
--- submissions/null/MyCoding.java
+++
@@ -1,7 +0,0 @@
-class MyCoding
-{
-	public static void main(String[] args)
-	{
-		System.out.println("301");
-	}
-}
