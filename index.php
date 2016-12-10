ae3ee98a9a2f3eb7dd7511376b507dd0a5f5389a
arpanpathak <arpanpathak1996@gmail.com>
2016-12-10 18:07:38 +0530

    First Commit

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <head>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<script type="text/javascript" src="script/jquery.js"></script>
 	<script src="css/bootstrap/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
